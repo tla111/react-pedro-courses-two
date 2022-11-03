48ffac356037ff15b569d5952eb34e85f7e3d4ff
Tim La <tlamarketing111@gmail.com>
2022-11-03 11:42:36 -0400

    implemented person component with prop values

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Person
+        name="Pedro"
+        email="pedro@gmail.com"
+        age={21}
+        isMarried={true}
+        friends={["jessica", "jake", "jerry", "jasmine"]}
+      />
     </div>
   );
 }
