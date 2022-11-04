98929e4a4dea32abde0e8894b20c9c30bb98bda2
Tim La <tlamarketing111@gmail.com>
2022-11-04 10:47:20 -0400

    created userSlice using createSlice

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,3 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
 
-export const store = configureStore({})
+const initialState = { value: { username: "" } }
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.value = action.payload
+        },
+        logout: (state) => {
+            state = initialState
+        }
+    },
+})
+
+export const { login, logout } = userSlice.actions
+
+export const store = configureStore({
+    reducer: {
+        user: userSlice.reducer,
+    }
+})
