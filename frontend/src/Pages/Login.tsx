1e6a90903ddf55dad3163af00689ecb1e79fa0e7
Tim La <tlamarketing111@gmail.com>
2022-11-04 10:53:02 -0400

    able to display state using useSelector

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -1,12 +1,18 @@
 import { useState } from "react"
+import { login, logout } from '../store'
+import { useDispatch, useSelector } from "react-redux"
 
 export const Login = () => {
     const [newUsername, setNewUsername] = useState("")
+
+    const dispatch = useDispatch()
+    const username = useSelector((state: any) => state.user.value.username);
+
     return (
         <h1>
-            THIS IS THE LOGIN PAGE
+            {username}
             <input onChange={(e) => { setNewUsername(e.target.value) }} />
-            <button>Submit Login</button>
+            <button onClick={() => dispatch(login({ username: newUsername }))}>Submit Login</button>
             <button>Logout</button>
         </h1>
     )
