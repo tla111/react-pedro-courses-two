25a5d137bc625335fd360a57ca3db1e866f6d1e6
Tim La <tlamarketing111@gmail.com>
2022-11-04 10:49:48 -0400

    created state newUsername

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react"
+
 export const Login = () => {
+    const [newUsername, setNewUsername] = useState("")
     return (
         <h1>
             THIS IS THE LOGIN PAGE
-            <input />
+            <input onChange={(e) => { setNewUsername(e.target.value) }} />
             <button>Submit Login</button>
             <button>Logout</button>
         </h1>
