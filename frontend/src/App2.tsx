52f274d82d3d2e56d50783b20241e61161d3a0ef
Tim La <tlamarketing111@gmail.com>
2022-11-04 10:31:54 -0400

    used boilerplate code in app

--- frontend/src/App2.tsx
+++ frontend/src/App2.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { Login } from "./pages/Login";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-
-const App2 = () => {
+function App() {
     return (
-        <div className="App">App2</div>
-    )
+        <div className="App">
+            <Provider store={store}>
+                <Router>
+                    <Link to="/"> Home </Link>
+                    <Link to="/login"> Login </Link>
+                    <Link to="/contact"> Contact </Link>
+
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/contact" element={<Contact />} />
+                    </Routes>
+                </Router>
+            </Provider>
+        </div>
+    );
 }
 
-export default App2
+export default App;
