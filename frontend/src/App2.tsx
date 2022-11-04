e5e0f2e329494995fba06d52235624144cce2c71
Tim La <tlamarketing111@gmail.com>
2022-11-04 10:40:04 -0400

    created store & configure

--- frontend/src/App2.tsx
+++ frontend/src/App2.tsx
@@ -2,25 +2,25 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { Contact } from "./Pages/Contact";
 import { Login } from "./Pages/Login";
-// import { Provider } from "react-redux";
-// import { store } from "./store";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 function App() {
     return (
         <div className="App">
-            {/* <Provider store={store}> */}
-            <Router>
-                <Link to="/"> Home </Link>
-                <Link to="/login"> Login </Link>
-                <Link to="/contact"> Contact </Link>
+            <Provider store={store}>
+                <Router>
+                    <Link to="/"> Home </Link>
+                    <Link to="/login"> Login </Link>
+                    <Link to="/contact"> Contact </Link>
 
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/contact" element={<Contact />} />
-                </Routes>
-            </Router>
-            {/* </Provider> */}
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/contact" element={<Contact />} />
+                    </Routes>
+                </Router>
+            </Provider>
         </div>
     );
 }
